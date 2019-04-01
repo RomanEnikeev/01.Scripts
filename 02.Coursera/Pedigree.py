28fd24487cda264aeafa1d2fdb82f7dd402d7709
RomanEnikeev <enikeevromang@gmail.com>
2019-04-01 10:38:51 +0300

    Для сдачи с input()

--- 02.Coursera/Pedigree.py
+++ 02.Coursera/Pedigree.py
@@ -7,20 +7,18 @@ def family_member(name, level, dict, father):
             return family_member(i, level, dict, father)
 
 
-
 fin = open('input.txt', 'r', encoding='utf8')
-#count = int(input())
-count = int(fin.readline()[:-1])
+count = int(input())
 
 dict = {}
 inital = list()
 
 for i in range(count - 1):
-    line = fin.readline()[:-1].split()
+    line = input().split()
     son = line[0]
     parent = line[1]
 
-#    print(line)
+    #    print(line)
     if son not in inital:
         inital.append(son)
     if parent not in inital:
@@ -29,18 +27,15 @@ for i in range(count - 1):
         dict[parent] = []
 
     dict[parent].append(son)
-    # if son not in dict:
-    #     dict[son] = []
 
-i = 0
-#поиск старшего
+lenDict = len(dict)
 for parent in dict:
+    i = 0
     for find in dict:
         if parent not in dict[find]:
             i += 1
-    if i == len(dict):
+    if i == lenDict:
         father = parent
 result = list()
 for i in sorted(inital):
     print(' '.join(map(str, [i, family_member(i, 0, dict, father)])))
-
