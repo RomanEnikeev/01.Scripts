3e8e1eecaa5f27f48d2b20ea420d6f21bafedd66
r.enikeev <enikeevromang@gmail.com>
2020-01-28 18:33:53 +0300

    Для сдачи с input()

--- 01.Scripts/06.RenameNameInEAEcsvFiles/RenameNameEAE.py
+++ 01.Scripts/06.RenameNameInEAEcsvFiles/RenameNameEAE.py
@@ -2,39 +2,59 @@ import os
 
 # папка с файлами
 dir = r'C:\Users\r.enikeev\Desktop\csvParsing\2015\tables'
-
+iskl = list()
 files = os.listdir(dir)
 pechat = list()
 
 for file in files:
-    way = dir + '\\' + file
-    inFile = open(way, 'r', encoding='windows-1251')
+    try:
+        way = dir + '\\' + file
+        inFile = open(way, 'r', encoding='windows-1251')
 
-    for line in inFile:
-        if line != '':
-            line = line.split(';')[:-1]
+        for line in inFile:
+            if line != '':
+                line = line[:-1].split(';')
 
-            bar_material = line[1]
-            el_current_code = line[2]
-            el_current = line[3]
-            if bar_material == '1' and el_current == '5000':
-                line[2] = '51'                                     #el_current_code для алюминия 5000 А
-            elif bar_material == '2' and el_current == '5000':
-                line[2] = '50'                                     #el_current_code для меди 5000 А
-            elif bar_material == '2' and el_current == '2500':
-                line[2] = '27'                                     #el_current_code для меди 2500 А
+                bar_material = line[1]
+                el_current_code = line[2]
+                el_current = line[3]
 
+                if bar_material == '1' and el_current == '5000':
+                    el_current_code = '51'  # el_current_code для алюминия 5000 А
+                    line[2] = el_current_code
 
-            outline = ';'.join(line)
+                elif bar_material == '2' and el_current == '5000':
+                    el_current_code = '50'  # el_current_code для меди 5000 А
+                    line[2] = el_current_code
 
-            if bar_material == '2' and el_current_code == '21' and el_current == '2250':     #так удаляем (в данном случае не добавляем) строки с данными параметрами
-                pass
-            elif bar_material == '2' and el_current_code == '43' and el_current == '4250':
-                pass
-            else:
-                pechat.append(outline)
-    inFile.close()
-    outFile = open(way, 'w', newline='')
-    for line in pechat:
-        print(line, file=outFile)
-    outFile.close()
+                elif bar_material == '2' and el_current == '2500':
+                    el_current_code = '27'  # el_current_code для меди 2500 А
+                    line[2] = el_current_code
+
+                outline = ';'.join(line)
+
+                if bar_material == '2' and el_current_code == '21' and el_current == '2250':  # так удаляем (в данном случае не добавляем) строки с данными параметрами
+                    pass
+                elif bar_material == '2' and el_current_code == '43' and el_current == '4250':
+                    pass
+                else:
+                    pechat.append(outline)
+        inFile.close()
+
+        outFile = open(way, 'w', newline='')
+        for line in pechat:
+            print(line, file=outFile)
+        outFile.close()
+        pechat = list()
+    except:
+        iskl.append(file)
+        try:
+            inFile.close()
+        except:
+            pass
+        try:
+            outFile.close()
+        except:
+            pass
+
+print(iskl)
