0c1d13c9525fdfc047a6f42fd2d7606cee8979d7
r.enikeev <enikeevromang@gmail.com>
2020-10-14 16:00:11 +0300

    notebooks

--- 01.Scripts/09.CreateFolder/array.py
+++ 01.Scripts/09.CreateFolder/array.py
@@ -1,3 +1,5 @@
+import shutil
+from tkinter import *
 import os
 
 shablons = {
@@ -6,34 +8,76 @@ shablons = {
 
 }
 
-files = {
-    'Семейства': [['00_ID', 'test11.txt'], ['00_ID', 'test21.txt'],
-                  ['01_КП', 'test31.txt'], ['01_КП', 'test41.txt']
-                  ],
-    'Моделирование': [['00_ID', 'test12.txt'], ['00_ID', 'test22.txt'],
-                  ['01_КП', 'test32.txt'], ['01_КП', 'test42.txt']
-                  ],
+# files = {
+#     'Семейства': [['00_ID', 'test11.txt'], ['00_ID', 'test21.txt'],
+#                   ['01_КП', 'test31.txt'], ['01_КП', 'test41.txt']
+#                   ],
+#     'Моделирование': [['00_ID', 'test12.txt'], ['00_ID', 'test22.txt'],
+#                   ['01_КП', 'test32.txt'], ['01_КП', 'test42.txt']
+#                   ],
+#
+#
+#
+# }
+
+def copy(directory, new_directory):
+    #нормализованный путь к файлам
+    directory = os.path.abspath(directory)
+
+    #файлы в папке
+    object_in_folder = os.listdir(directory)
+
+    #перебираем объекты и проверяем: файл или папка
+    for object in object_in_folder:
+        way = directory + '\\' + object
+
+        #если путь ведет к файлу:
+        if os.path.isfile(way) == True:
+            file = object
+            try:
+                new_directory = path + '\\' + '\\'.join(way.split('\\')[-2:])
+                shutil.copyfile(way, new_directory)
+            except:
+                pass
+
+            # #есть ли файл в списке
+            # for i in allNames:
+            #     if i[1] == file:
+            #         print(file)
+            #         name_new = i[0]
+            #         try:
+            #             os.rename(directory + '\\' + file, directory + '\\' + name_new)
+            #             break
+            #         except FileExistsError:
+            #             break
+        #если путь ведет к папке
+        else:
+            copy(way, new_directory)
 
 
 
-}
-# print(path)
 
 
-from tkinter import *
+source_ways = r"\\dc\Bim\08_Общая\00_BIM_JUNIOR\03_Рабочая\Roma\createfolder"
+
+print(os.listdir(source_ways))
+
+
+
 
 root = Tk()
 root.title("Графическая программа на Python")
 root.geometry("400x300")
 
-entry = Entry(width=20)
-create = Button(text="Создать", command=root.destroy)
 
+entry = Entry(width=20)                                     # текстовое поле ввода
+create = Button(text="Создать", command=root.destroy)       # кнопка создать
 
+# выскакивающее меню
 variable = StringVar(root)
-variable.set(list(files.keys())[0]) # default value
-print(variable.set())
-w = OptionMenu(root, variable, *list(files.keys()))
+variable.set(list(shablons.keys())[0]) # default value
+
+w = OptionMenu(root, variable, *list(shablons.keys()))
 
 
 def entrydirectory(event):
@@ -43,6 +87,8 @@ def entrydirectory(event):
 
 
     # return directory
+
+
 
 
 create.bind('<Button-1>', entrydirectory)
@@ -64,12 +110,12 @@ root.mainloop()
 
 
 
-
+tag = variable.get()  # забираем из выпадающего меню тег
 # path = r'C:\Users\r.enikeev\Desktop\Новая папка'
 
 
 
-tag = 'Моделирование'
+# tag = 'Моделирование'
 
 
 
@@ -77,10 +123,13 @@ try:
     for folder in shablons[tag]:
         directory_of_folder = path + '\\' + folder
         os.mkdir(directory_of_folder)
-
-
-
 except OSError:
     print("Creation of the directory %s failed" % path)
 else:
     print("Successfully created the directory %s " % path)
+
+
+print(directory_of_folder)
+copy(source_ways, path)
+
+
